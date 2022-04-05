d068d61189e9786fe797e980f807c12968c54d69
Nick Mills-Barrett <nick@beeper.com>
2022-04-05 15:13:40 +0100

    Add `--port` and `--host` CLI flags

--- dummy_bridge/__main__.py
+++ dummy_bridge/__main__.py
@@ -6,14 +6,26 @@ import logging
 from . import DummyBridge
 
 
-async def async_main(homeserver, owner, registration_file):
+async def async_main(
+    homeserver,
+    owner,
+    registration_file,
+    port,
+    host,
+):
     logging.basicConfig(level=logging.WARNING)
     logging.getLogger("dummy_bridge").setLevel(level=logging.TRACE)
 
     with open(registration_file, "r") as f:
         registration = json.load(f)
 
-    bridge = DummyBridge(homeserver, registration, owner)
+    bridge = DummyBridge(
+        homeserver_url=homeserver,
+        registration=registration,
+        owner=owner,
+        listen_host=host,
+        listen_port=port,
+    )
     await bridge.bootstrap()
     await asyncio.Event().wait()
 
@@ -22,6 +34,8 @@ async def async_main(homeserver, owner, registration_file):
 @click.argument("homeserver")
 @click.argument("owner")
 @click.argument("registration_file")
+@click.option("--port", type=int, default=5000)
+@click.option("--host", default="127.0.0.1")
 def main(*args, **kwargs):
     asyncio.run(async_main(*args, **kwargs))
 
